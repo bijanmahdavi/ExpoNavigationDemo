1ec1359dcca4d399f37208ecfde6fb706d79df26
Bijan Mahdavi <bijmahdavi@gmail.com>
2020-09-09 08:21:39 -0700

    changed base stack nav to include tab navigation

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { SetValuePass } from './OurTextInput.js'
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import FlatListt, { Welcome } from './FlatList.js'
 import MovieList from './MovieList.js'
 
@@ -34,30 +36,102 @@ function LoginScreen({ navigation }) {
   );
 }
 
-function HomeScreen() {
+function HomeScreen2() {
   return (
     <View style={styles.container}>
     <MovieList></MovieList>
     </View>)
+}
+
+function DetailsScreen2() {
+  return (
+    <View style={styles.container}>
+    <MovieList></MovieList>
+    </View>)
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>  
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
 }
 
 function DetailsScreen() {
   return (
-    <View style={styles.container}>
-    <MovieList></MovieList>
-    </View>)
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
 }
 
 const Stack = createStackNavigator();
 
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator>
+     <Stack.Screen name="Home" component={HomeScreen} />
+     <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+   );
+ }
+
+const SettingsStack = createStackNavigator();
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
 <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Details" component={DetailsScreen}/>
-      </Stack.Navigator>
+<Tab.Navigator
+       screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+         let iconName;
+         if (route.name === 'Home') {
+            iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused
+            ? 'ios-list-box'
+            : 'ios-list';
+          }
+    
+     return <Ionicons name={iconName} size={size} color={color}     />;
+       },
+    })}
+tabBarOptions={{
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'gray',
+    }}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component=     {SettingsStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
